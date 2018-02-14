d3326d5fcd6d3b77aac6ad0ae196154f592670a2
Yuuuu <abc@gmail.com>
2018-02-14 14:31:59 +0000

    Error repair

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,13 @@ class FavoritesController < ApplicationController
     user = User.find(params[:micropost_id])
     current_user.favorite(user)
     flash[:success] = 'お気に入りに追加しました。'
-    redirect_to @user
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     user = User.find(params[:micropost_id])
     current_user.unfavorite(user)
     flash[:success] = 'お気に入りを解除しました。'
-    redirect_to @user
+    redirect_back(fallback_location: root_path)
   end
 end
